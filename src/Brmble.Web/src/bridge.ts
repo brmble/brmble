ef63f952d8f234c5e93a98f6a40d83e9f5400550
Roan Wery <rwe@afasgroep.nl>
2026-02-15 11:57:31 +0100

    feat: add JavaScript bridge module for C# communication

---
+++ src/Brmble.Web/src/bridge.ts
@@ -0,0 +1,57 @@
+type MessageHandler = (data: unknown) => void;
+
+const bridge = {
+  _handlers: new Map<string, MessageHandler[]>(),
+
+  init() {
+    const webview = window.chrome?.webview;
+    if (webview) {
+      webview.addEventListener('message', (event: { data: { type: string; data?: unknown } }) => {
+        this._handleMessage(event);
+      });
+    }
+  },
+
+  _handleMessage(event: { data: { type: string; data?: unknown } }) {
+    try {
+      const { type, data } = event.data;
+      console.log('[JS Bridge] Received:', type, data);
+      
+      if (this._handlers.has(type)) {
+        this._handlers.get(type)?.forEach(handler => handler(data));
+      }
+    } catch (e) {
+      console.error('[JS Bridge] Error:', e);
+    }
+  },
+
+  send(type: string, data: unknown = null) {
+    const webview = window.chrome?.webview;
+    if (webview) {
+      webview.postMessage({ type, data });
+    } else {
+      console.warn('[JS Bridge] Not running in WebView2');
+    }
+  },
+
+  on(type: string, handler: MessageHandler) {
+    if (!this._handlers.has(type)) {
+      this._handlers.set(type, []);
+    }
+    this._handlers.get(type)?.push(handler);
+  },
+
+  off(type: string, handler: MessageHandler) {
+    if (this._handlers.has(type)) {
+      const handlers = this._handlers.get(type);
+      const index = handlers?.indexOf(handler) ?? -1;
+      if (index > -1) {
+        handlers?.splice(index, 1);
+      }
+    }
+  }
+};
+
+bridge.init();
+
+export default bridge;
